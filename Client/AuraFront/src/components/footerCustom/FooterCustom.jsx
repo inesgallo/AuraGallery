d06b0c6fa00b067d97bbce2d53204389c5d5cd7a
Yamila <yamilarobles10@gmail.com>
2024-04-11 13:05:03 +0200

    add bsic structure

---
+++ Client/AuraFront/src/components/footerCustom/FooterCustom.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footerCustom.css'
+
+const FooterCustom = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FooterCustom
--- Client/AuraFront/src/main.jsx
+++ Client/AuraFront/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarCustom from './components/navbarCustom/NavbarCustom.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<NavbarCustom />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
