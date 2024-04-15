2d91b7bdd09e0c8d6d8f4feaeeeb1d8647340c47
Yamila <yamilarobles10@gmail.com>
2024-04-15 21:05:51 +0200

    add db.json

--- Client/AuraFront/src/services/UserService.js
+++ Client/AuraFront/src/services/UserService.js
@@ -1,11 +1,11 @@
 const users = [
-    { username: 'admin@gmail.com', password: 'admin123', userType: 'admin' },
-    { username: 'artist1.@gmail.com', password: 'artist123', userType: 'artist' },
-    { username: 'client1@gmail.com', password: 'client123', userType: 'client' },
+    { email: 'admin@gmail.com', password: 'admin123', userType: 'admin' },
+    { email: 'artist1.@gmail.com', password: 'artist123', userType: 'artist' },
+    { email: 'client1@gmail.com', password: 'client123', userType: 'client' },
    ];
    
    export const authenticateUser = (username, password) => {
-    const user = users.find(user => user.username === username && user.password === password);
+    const user = users.find(user => user.email === username && user.password === password);
     return user ? { success: true, userType: user.userType } : 
     { success: false };
    };
