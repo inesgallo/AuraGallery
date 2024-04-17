ff87c6c8fc6dcf9c75c516d5a67963d1b6c9e39e
GabrielaRamos79 <gabyramos@me.com>
2024-04-17 14:43:41 +0200

    Added get method to product service

--- Back/src/__init__.py
+++ Back/src/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .routes import user_route
 from .routes import auth_route
+from .routes import product_route
 from flask_cors import  CORS
 
 app = Flask( __name__ )
@@ -9,5 +10,6 @@ def init_app(config):
     app.config.from_object(config)
     app.register_blueprint(user_route.main, url_prefix='/user_admin')
     app.register_blueprint(auth_route.main, url_prefix='/login')
+    app.register_blueprint(product_route.main, url_prefix='/product')
     #app.register_blueprint(personRouter.main, url_prefix='/person')
     return app
---
+++ Back/src/services/product_service.py
@@ -0,0 +1,81 @@
+from src.database.db_phpMySql import get_connection
+# from werkzeug.security import generate_password_hash
+from src.models.product_model import  Product
+# from werkzeug.security import generate_password_hash
+
+class ProductService():
+    @classmethod
+    def get_product(cls):
+        try:
+            connection  = get_connection()
+            print(connection)
+            with connection.cursor() as cursor:
+                # cursor.execute('SELECT * FROM product')
+                cursor.callproc('sp_get_product')
+                result = cursor.fetchall()
+                print(result)
+            connection.close()
+            return result
+        except Exception as ex:
+            print(ex)
+    # @classmethod
+    # def post_user(cls, user_table:User):
+    #     try:
+    #         connection  = get_connection()
+    #         #print(connection)
+    #         # id_user = user_table.id_user
+    #         name_user = user_table.name_user
+    #         password_user = user_table.password_user
+    #         user_typeFK = user_table.user_typeFK
+            
+    #         encriped_password = generate_password_hash(password_user, 'pbkdf2', 30)
+            
+    #         with connection.cursor() as cursor:
+                
+    #             # cursor.execute("INSERT INTO user(id_user, name_user, password_user, id_user_typeFK) VALUES ({0}, '{1}', '{2}', {3})"
+    #                         #    .format(id_user,name_user,password_user,user_typeFK))
+    #             cursor.callproc('post_user', (name_user,encriped_password,user_typeFK))
+    #             connection.commit()
+    #             print('User added successfully')
+    #         connection.close()
+    #         return "Data base is close"
+    #     except Exception as ex:
+    #         print(ex)
+    # @classmethod
+    # def patch_user(cls, user_table:User):
+    #     try:
+    #         connection  = get_connection()
+    #         id_user = user_table.id_user
+    #         name_user = user_table.name_user
+    #         password_user = user_table.password_user
+    #         user_typeFK = user_table.user_typeFK
+            
+    #         encriped_password = generate_password_hash(password_user, 'pbkdf2', 30)
+            
+    #         with connection.cursor() as cursor:
+    #             # cursor.execute("UPDATE user SET  name_user = '{0}', password_user = '{1}', id_user_typeFK = {2}  WHERE user.id_user = {3}".format(name_user,password_user,user_typeFK,id_user))
+    #             cursor.callproc('update_user', (id_user,name_user,encriped_password,user_typeFK))
+    #             connection.commit()
+    #             print('User updated successfully')
+    #         connection.close()
+    #         return "Data base is close"
+    #     except Exception as ex:
+    #         print(ex)
+    # @classmethod
+    # def delete_user(cls, id_user):
+    #     try:
+    #         connection  = get_connection()
+    #         print(connection)
+    #         with connection.cursor() as cursor:
+    #             # cursor.execute('DELETE FROM user WHERE user.id_user = %s', (id_user)) 
+    #             cursor.callproc('delete_user', (id_user,)) # Aqui uso otro metodo callproc para trabajar con procedimientos
+    #             connection.commit()
+    #         connection.close()
+    #         return "Data base is close"
+    #     except Exception as ex:
+    #         print(ex)
+
+
+
+
+
