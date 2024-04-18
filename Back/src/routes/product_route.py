90648934954deffb43ad87df95399d823868b407
GabrielaRamos79 <gabyramos@me.com>
2024-04-18 19:04:07 +0200

    Added the last two methods to product PATCH and DELETE

--- Back/src/routes/product_route.py
+++ Back/src/routes/product_route.py
@@ -20,20 +20,21 @@ def manage_user():
         product_table = Product(0,title_product,image_product,category_product, description_product, stock_product, price_product, user_artistFK)
         post_product = ProductService.post_product(product_table)
         print(post_product)
-    # elif request.method == "PATCH":
-    #     id_user = request.json['id_user']
-    #     name_user = request.json['name_user']
-    #     password_user = request.json['password_user']
-    #     user_typeFK = request.json['user_typeFK']
-    #     user_table = User(id_user,
-    #                     name_user,
-    #                     password_user,
-    #                     user_typeFK)
-    #     patch_user = UserService.patch_user(user_table)
-    #     print(patch_user)
-    # elif request.method == "DELETE":
-    #     id_user = request.json['id_user']
-    #     delete_user = UserService.delete_user(id_user)
-    #     print(delete_user)
+    elif request.method == "PATCH":
+        id_product = request.json['id_product']
+        title_product = request.json['title_product']
+        image_product = request.json['image_product']
+        category_product = request.json['category_product']
+        description_product = request.json['description_product']
+        stock_product = request.json['stock_product']
+        price_product = request.json['price_product']
+        user_artistFK = request.json['id_user_artistFK']
+        product_table = Product(id_product,title_product,image_product,category_product, description_product, stock_product, price_product, user_artistFK)
+        patch_product = ProductService.patch_product(product_table)
+        print(patch_product)
+    elif request.method == "DELETE":
+        id_product = request.json['id_product']
+        delete_product = ProductService.delete_product(id_product)
+        print(delete_product)
     print('Esto se imprime en consola')
     return 'Esto se ve en la pagina'
--- Back/src/services/product_service.py
+++ Back/src/services/product_service.py
@@ -14,7 +14,7 @@ class ProductService():
                 cursor.callproc('sp_get_product')
                 result = cursor.fetchall()
                 # [Product.convert_desde_BD(fila) for fila in result]
-                products_json = [{"id_product": row[0], "title_product": row[1], "image_product": row[2], "category_product": row[3], "description_product": row[4], "size_product": row[5], "price_product": row[6], "id_user_artistFK": row[7]} for row in result]
+                products_json = [{"id_product": row[0], "title_product": row[1], "image_product": row[2], "category_product": row[3], "description_product": row[4], "stock_product":row[5], "price_product": row[6], "id_user_artistFK": row[7]} for row in result]
                 print(result)
             connection.close()
             return products_json
@@ -33,7 +33,7 @@ class ProductService():
             # price_product = product_table.price_product,
             # id_user_artistFK = product_table.user_artistFK
             with connection.cursor() as cursor:
-                cursor.callproc('	sp_post_product', (
+                cursor.callproc('sp_post_product', (
             #     sql = "INSERT INTO product(id_product, title_product, image_product, category_product, description_product, stock_product, price_product, id_user_artistFK) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
             #     cursor.execute(sql, (
                     # product_table.id_product,
@@ -57,39 +57,47 @@ class ProductService():
             connection.close()
             return "Data base is close"
        
-    # @classmethod
-    # def patch_user(cls, user_table:User):
-    #     try:
-    #         connection  = get_connection()
-    #         id_user = user_table.id_user
-    #         name_user = user_table.name_user
-    #         password_user = user_table.password_user
-    #         user_typeFK = user_table.user_typeFK
+    @classmethod
+    def patch_product(cls, product_table:Product):
+        try:
+            connection  = get_connection()
+            id_product = product_table.id_product
+            title_product = product_table.title_product
+            image_product = product_table.image_product
+            category_product = product_table.category_product
+            description_product = product_table.description_product
+            stock_product = product_table.stock_product
+            price_product = product_table.price_product
+            user_artistFK = product_table.user_artistFK
             
     #         encriped_password = generate_password_hash(password_user, 'pbkdf2', 30)
             
-    #         with connection.cursor() as cursor:
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    UPDATE product 
+                    SET title_product = %s, image_product = %s, category_product = %s, description_product = %s, stock_product = %s, price_product = %s, id_user_artistFK = %s 
+                    WHERE product.id_product = %s""", (title_product, image_product, category_product, description_product, stock_product, price_product, user_artistFK, id_product))
     #             # cursor.execute("UPDATE user SET  name_user = '{0}', password_user = '{1}', id_user_typeFK = {2}  WHERE user.id_user = {3}".format(name_user,password_user,user_typeFK,id_user))
     #             cursor.callproc('update_user', (id_user,name_user,encriped_password,user_typeFK))
-    #             connection.commit()
-    #             print('User updated successfully')
-    #         connection.close()
-    #         return "Data base is close"
-    #     except Exception as ex:
-    #         print(ex)
-    # @classmethod
-    # def delete_user(cls, id_user):
-    #     try:
-    #         connection  = get_connection()
-    #         print(connection)
-    #         with connection.cursor() as cursor:
-    #             # cursor.execute('DELETE FROM user WHERE user.id_user = %s', (id_user)) 
-    #             cursor.callproc('delete_user', (id_user,)) # Aqui uso otro metodo callproc para trabajar con procedimientos
-    #             connection.commit()
-    #         connection.close()
-    #         return "Data base is close"
-    #     except Exception as ex:
-    #         print(ex)
+                connection.commit()
+                print('User updated successfully')
+            connection.close()
+            return "Data base is close"
+        except Exception as ex:
+            print(ex)
+    @classmethod
+    def delete_product(cls, id_product):
+        try:
+            connection  = get_connection()
+            print(connection)
+            with connection.cursor() as cursor:
+                # cursor.execute('DELETE FROM product WHERE product.id_product = %s', (id_product)) 
+                cursor.callproc('delete_product', (id_product,)) # Aqui uso otro metodo callproc para trabajar con procedimientos
+                connection.commit()
+            connection.close()
+            return "Data base is close"
+        except Exception as ex:
+            print(ex)
 
 
 
